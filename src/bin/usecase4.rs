b9af16c053bea8c9060ae41addee27361630c5df
Kong Lingkun <klk316980786@sjtu.edu.cn>
2022-08-21 22:46:58 -0500

    set up examples to be tested

--- src/bin/usecase4.rs
+++ src/bin/usecase4.rs
@@ -1,3 +1,8 @@
-fn main() {
+use hashmap::dummy::*;
 
+fn main() {
+    let _timber_resources: HashMap<&str, i32> = [("Norway", 100), ("Denmark", 50), ("Iceland", 10)]
+        .iter()
+        .cloned()
+        .collect();
 }
